164d2867b91302a86881cfec315a7260217df791
jscallen11287 <callenjs@gmail.com>
2016-10-19 14:02:56 -0400

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,12 @@
+var nav = '<div id="fftitle">Estates Fantasy Football League</div>'+
+	'<div id="ulDiv">'+
+		'<ul id="navul">'+
+		 '<li><a class="normala" href="http://www.estatesffl.com/last-man-standing-1.html">Last Man Standing</a></li>'+
+		  '<li><a class="normala" href="http://www.estatesffl.com/effl-home.html">EFFL Home</a></li>'+
+		  '<li><a class="normala" href="http://www.estatesffl.com/monday-night-football.html">Monday Night Football</a></li>'+
+		  '<li><a class="normala" href="http://www.estatesffl.com/finances.html">Finances</a></li>'+
+		  '<li><a class="normala" href="http://www.estatesffl.com/super-bowl.html">Super Bowl</a></li>'+
+		  '<li class="redli"><a class="reda" href="http://rtsports.com/" target="_blank">RT Sports</a></li>'+
+		  '<li class="redli"><a class="reda" href="http://poolhost.com/" target="_blank">Weekly Pools</a></li>'+
+		'</ul>'+
+	'</div>';
